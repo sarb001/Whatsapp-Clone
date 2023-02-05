d37b018fab6d85e4f27c4efad12e127146ce191d
SARB <mrsinghbusiness05@gmail.com>
2023-02-05 20:33:03 +0530

    'Updated'

--- backend/Controllers/JsoncreateToken.js
+++ backend/Controllers/JsoncreateToken.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 // Able to Create Token Now 
 const jsoncreatetoken = (id) => {
     
-     return  jwt.sign( {id}, process.env.JWT_SECRET , {
+     return  jwt.sign( {id}, process.env.JWT_SECRET ,  {
             expiresIn:'30d',
     })
 }
